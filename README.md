c375687cd99d2b1fc85d7e303aa4e3d44ac8dac7
mitchnode <70460735+mitchnode@users.noreply.github.com>
2024-01-07 21:55:08 +1100

    Added New button and randomised colour
    New button prompts for grid size between 1 and 100 and generates grid.
    Sqaures change to random colour on hover.

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Etch-a-sketch project for the Odin Project
 16x16 grid of square divs
 size of square calculated 1000/grid size
 
+To Do:
+Add reset button
+Add prompt for new grid size
+
+Randomize color change
+incrementally darker squares
--- sketcher.js
+++ sketcher.js
@@ -1,6 +1,16 @@
 let grid = 16;
+let r = 200;
+let g = 200;
+let b = 200;
 
+
+const page = document.querySelector('.page');
 const container = document.querySelector('.container');
+
+
+function randomColor(){
+    
+}
 
 function createGrid(grid){
     let size = 1000/grid;
@@ -10,10 +20,29 @@ function createGrid(grid){
             newSquare.classList = 'square new';
             newSquare.setAttribute("style","width:" + size + "px; height: " + size + "px");
             newSquare.addEventListener('mouseover', function () {
-                newSquare.classList = 'square sketched';
+                //newSquare.classList = 'square sketched';
+                r = Math.random() * 255;
+                g = Math.random() * 255;
+                b = Math.random() * 255;
+                newSquare.setAttribute("style","width:" + size + "px; height: " + size + "px; background-color: rgba(" + r + "," + g + "," + b + ",20)");
             });
             container.appendChild(newSquare);
         }
     }
 }
+
+const newButton = document.createElement('button');
+newButton.textContent = "New";
+container.before(newButton);
+newButton.addEventListener('click',function (){
+    grid = 0;
+    while (grid > 100 || grid <= 0){
+        grid = Number(prompt('Enter Grid Size (1-100):',16));
+    }
+    while( container.hasChildNodes()){
+        container.removeChild(container.lastChild);
+    }  
+    createGrid(grid);
+})
+
 createGrid(grid);
